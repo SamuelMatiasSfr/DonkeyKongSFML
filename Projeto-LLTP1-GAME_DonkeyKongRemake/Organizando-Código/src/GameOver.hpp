f4c8fdfe6fe197a321e0cae178bea0f182a17276
Samuel Matias <136845273+SamuelMatiasSfr@users.noreply.github.com>
2024-10-07 23:06:38 -0300

    Update GameOver.hpp

--- Projeto-LLTP1-GAME_DonkeyKongRemake/Organizando-Código/src/GameOver.hpp
+++ Projeto-LLTP1-GAME_DonkeyKongRemake/Organizando-Código/src/GameOver.hpp
@@ -1,16 +1,39 @@
 #ifndef GAMEOVER_HPP_
 #define GAMEOVER_HPP_
 
-#include "Mapa.hpp"
 #include "Entidade.hpp"
-#include "Fase1.hpp"
+#include "Jogo.hpp"
 #include "Credito.hpp"
 
 class GameOver {
 
+private:
+	sf::Texture texturaFundoGameOver;
+	sf::Texture texturaBotaoPlay;
+	sf::Texture texturaBotaoPlayHover;
+	sf::Texture texturaBotaoCredito;
+	sf::Texture texturaBotaoCreditoHover;
+
+	sf::Music musicaGameOver;
+
+	sf::Font fonteTituloRemake;
+	sf::Text tituloRemake;
+
+	sf::Sprite fundoGameOver;
+	sf::VideoMode tamanhoJanela;
+	sf::Event event;
+	sf::Image icon;
+
+	Mapa *mapaGameOver;
+	Entidade *botaoPlay;
+	Entidade *botaoCredito;
+
 public:
 	GameOver();
 	~GameOver();
+
+    void reiniciarJogo();
+    void mostrarCreditos();
 
 };
 
