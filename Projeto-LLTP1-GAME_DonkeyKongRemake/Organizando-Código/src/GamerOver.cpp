333feaac53d8550282c24670ce6c5d2ee770905b
Samuel Matias <136845273+SamuelMatiasSfr@users.noreply.github.com>
2024-10-07 23:05:57 -0300

    Update GamerOver.cpp

--- Projeto-LLTP1-GAME_DonkeyKongRemake/Organizando-Código/src/GamerOver.cpp
+++ Projeto-LLTP1-GAME_DonkeyKongRemake/Organizando-Código/src/GamerOver.cpp
@@ -1,118 +1,139 @@
 #include "GameOver.hpp"
 
 GameOver::GameOver() {
-	sf::Texture texturaFundoGameOver;
-	if (!texturaFundoGameOver.loadFromFile("imagens/fundoCarregamento.jpg")) {
+	//cria e carrega fundo da tela game over
+	if(!texturaFundoGameOver.loadFromFile("imagens/fundoCarregamento.jpg")){
 		std::cerr << "Erro ao abrir a imagem de fundo\n";
 	}
-
-	sf::Sprite fundoGameOver;
 	fundoGameOver.setTexture(texturaFundoGameOver);
 
-	sf::VideoMode tamjanela(texturaFundoGameOver.getSize().x, texturaFundoGameOver.getSize().y);
-	sf::Event event;
 
-	sf::Image icon;
-	if (!icon.loadFromFile("imagens/donkeyKong-icon.png")) {
-		std::cerr << "Erro ao abrir a textura de �cone\n";
+	//cria a janela
+	tamanhoJanela = sf::VideoMode(texturaFundoGameOver.getSize().x, texturaFundoGameOver.getSize().y);
+
+
+	//cria e carrega o icone
+	if(!icon.loadFromFile("imagens/donkeyKong-icon.png")){
+		std::cerr << "Erro ao abrir a textura de icone\n";
 	}
 
-	Mapa mapaGameOver(tamjanela, fundoGameOver);
-	mapaGameOver.getWindow().setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+	//cria mapa de game over
+	mapaGameOver = new Mapa(tamanhoJanela, fundoGameOver);
+	mapaGameOver->getWindow().setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
 
-	sf::Texture texturaBotaoPlay[2];
-	if (!texturaBotaoPlay[0].loadFromFile("imagens/botaoJogar.png")) {
+	//carrega texturas dos botoes
+	if(!texturaBotaoPlay.loadFromFile("imagens/botaoJogar.png")){
 		std::cerr << "Erro ao abrir a textura do botao de jogar\n";
 	}
-	if (!texturaBotaoPlay[1].loadFromFile("imagens/botaoJogarHover.png")) {
+	if (!texturaBotaoPlayHover.loadFromFile("imagens/botaoJogarHover.png")) {
 		std::cerr << "Erro ao abrir a textura do botao jogar hover\n";
 	}
 
-	sf::Texture texturaBotaoCredito[2];
-	if (!texturaBotaoCredito[0].loadFromFile("imagens/botaoCreditos.png") ||
-			!texturaBotaoCredito[1].loadFromFile("imagens/botaoCreditosHover.png")) {
-		std::cerr << "Erro ao carregar texturas de botão Créditos." << std::endl;
+	if(!texturaBotaoCredito.loadFromFile("imagens/botaoCreditos.png") ||
+			!texturaBotaoCreditoHover.loadFromFile("imagens/botaoCreditosHover.png")){
+		std::cerr << "Erro ao carregar texturas de botao Creditos\n";
 	}
 
 
-
-	sf::Font fonteTituloRemake;
+	//carrega fontes
 	fonteTituloRemake.loadFromFile("fontes/fonte1.TTF");
 
-	sf::Text tituloRemake;
 	tituloRemake.setFont(fonteTituloRemake);
 	tituloRemake.setString("GAME OVER");
 	tituloRemake.setFillColor(sf::Color(111, 0, 0));
 	tituloRemake.setCharacterSize(60);
-	tituloRemake.setPosition(tamjanela.width/5, 200);
+	tituloRemake.setPosition(tamanhoJanela.width/5, 200);
 	tituloRemake.setOutlineThickness(3);
 	tituloRemake.setOutlineColor(sf::Color(162, 130, 0));
 
-	sf::Music musicaGameOver;
+
+	//carrega musica de game over
 	if (!musicaGameOver.openFromFile("audios/gameOver.flac")) {
 		std::cerr << "Erro ao carregar o arquivo de som\n";
 	}
 	musicaGameOver.setVolume(50);
 
-	sf::IntRect retangulo(0, 0, 300, 120);
-	sf::Vector2f escala(1.0f, 1.0f);
-	Entidade botaoPlay(texturaBotaoPlay[0], 400, 400, retangulo, escala);
-	Entidade botaoCredito(texturaBotaoCredito[0], 400, 550, retangulo, escala);
 
-	// Loop principal
-	while (mapaGameOver.getWindow().isOpen()) {
-		while (mapaGameOver.getWindow().pollEvent(event)) {
+	//cria entidades botoes
+	botaoPlay = new Entidade(texturaBotaoPlay, 400, 400, sf::IntRect (0, 0, 300, 120), sf::Vector2f (1.0f, 1.0f));
+	botaoCredito = new Entidade(texturaBotaoCredito, 400, 550, sf::IntRect (0, 0, 300, 120), sf::Vector2f (1.0f, 1.0f));
+
+
+	//loop principal
+	while (mapaGameOver->getWindow().isOpen()) {
+
+		//loop de eventos
+		while (mapaGameOver->getWindow().pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
-				mapaGameOver.getWindow().close();
+				mapaGameOver->getWindow().close();
 			}
 		}
 
-		sf::Vector2i mousePos = sf::Mouse::getPosition(mapaGameOver.getWindow());
-		mapaGameOver.setCordenadas(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
 
-		if (botaoPlay.getSprite().getGlobalBounds().contains(mapaGameOver.getCordenadas())) {
-			botaoPlay.setTexturaSprite(texturaBotaoPlay[1]);
+		//atualiza mundo
+
+		//pega a posicao do mouse e passa como coordenadas para o mapa
+		sf::Vector2i mousePos = sf::Mouse::getPosition(mapaGameOver->getWindow());
+		mapaGameOver->setCordenadas(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+
+		//se o mouse passar por cima dos botoes aparecera o o botao hover, senao aparecera o botao normal
+		if (botaoPlay->getSprite().getGlobalBounds().contains(mapaGameOver->getCordenadas())) {
+			botaoPlay->setTexturaSprite(texturaBotaoPlayHover);
 		} else {
-			botaoPlay.setTexturaSprite(texturaBotaoPlay[0]);
+			botaoPlay->setTexturaSprite(texturaBotaoPlay);
 		}
-		if (botaoCredito.getSprite().getGlobalBounds().contains(mapaGameOver.getCordenadas())) {
-			botaoCredito.setTexturaSprite(texturaBotaoCredito[1]);
+		if (botaoCredito->getSprite().getGlobalBounds().contains(mapaGameOver->getCordenadas())) {
+			botaoCredito->setTexturaSprite(texturaBotaoCreditoHover);
 		} else {
-			botaoCredito.setTexturaSprite(texturaBotaoCredito[0]);
+			botaoCredito->setTexturaSprite(texturaBotaoCredito);
 		}
 
+		//a classe Jogo ou Credito sera criada se houver click e se o mouse estiver em contato com os botoes
 		if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-			if(botaoPlay.getSprite().getGlobalBounds().contains(mapaGameOver.getCordenadas())){
-				mapaGameOver.~Mapa();
-				if(musicaGameOver.getStatus() == sf::Sound::Playing){
-					musicaGameOver.pause();
-				}
-				Fase1 fase1;
+			if(botaoPlay->getSprite().getGlobalBounds().contains(mapaGameOver->getCordenadas())){
+				reiniciarJogo();
+
 			}
-			else if (botaoCredito.getSprite().getGlobalBounds().contains(mapaGameOver.getCordenadas())) {
-				Credito mostraCreditos;
+			else if (botaoCredito->getSprite().getGlobalBounds().contains(mapaGameOver->getCordenadas())) {
+				mostrarCreditos();
 			}
 
 		}
-
-		mapaGameOver.getWindow().clear();
 
 		if(musicaGameOver.getStatus() != sf::Sound::Playing){
 			musicaGameOver.play();
 		}
 
-		mapaGameOver.getWindow().draw(fundoGameOver);
-		mapaGameOver.getWindow().draw(tituloRemake);
-		mapaGameOver.getWindow().draw(botaoPlay.getSprite());
-		mapaGameOver.getWindow().draw(botaoCredito.getSprite());
 
-		mapaGameOver.getWindow().display();
+		//desenha mundo
+
+		mapaGameOver->getWindow().clear();
+
+		mapaGameOver->getWindow().draw(fundoGameOver);
+		mapaGameOver->getWindow().draw(tituloRemake);
+		mapaGameOver->getWindow().draw(botaoPlay->getSprite());
+		mapaGameOver->getWindow().draw(botaoCredito->getSprite());
+
+		mapaGameOver->getWindow().display();
 
 	}
 }
 
+void GameOver::reiniciarJogo(){
+    if(musicaGameOver.getStatus() == sf::Sound::Playing){
+    	musicaGameOver.stop();
+    }
+	delete mapaGameOver;
+	new Jogo();
+}
+
 GameOver::~GameOver(){
 	std::cerr << "Tela de Game Over Destruida\n";
+}
+
+void GameOver::mostrarCreditos(){
+	new Credito();
 }
 
